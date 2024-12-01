c130e92b79e673f4b1f302a666ce8798e349e4a6
RyuSeungBum <tmdqja3405@cu.ac.kr>
2024-12-01 07:43:32 +0000

    hw1

---
+++ chap11/hw1/almhandler.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void alarmHandler(int signo);
+
+int main() {
+    struct sigaction sa;
+
+    sa.sa_handler = alarmHandler; 
+    sa.sa_flags = 0;             
+    sigemptyset(&sa.sa_mask);     
+
+    if (sigaction(SIGALRM, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    alarm(5);  
+    short i = 0;
+    while (1) {
+        sleep(1);
+        i++;
+        printf("%d second\n", i);
+    }
+    printf("end\n");
+}
+
+void alarmHandler(int signo) {
+    printf("Wake up\n");
+    exit(0);
+}
+
